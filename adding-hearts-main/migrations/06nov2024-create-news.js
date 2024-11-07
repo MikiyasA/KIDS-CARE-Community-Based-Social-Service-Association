46cc42993c3ac0e6144e4cec26b0839e1e1118da
Mikiyas Adere <88377649+MikiyasA@users.noreply.github.com>
2024-11-07 21:54:27 +0300

    push migration

---
+++ adding-hearts-main/migrations/06nov2024-create-news.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("News", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cover: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      detail: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      createdBy: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      updatedBy: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("News");
+  },
+};
